2af266fe80c83d07ad874f6cf438579e7f28ab22
sepinulacfjj <SepinulaCriCri@gmail.com>
2026-09-12 23:54:59 +0100

    Initial commit

---
+++ src/main/java/net/sepinula/talesofeldoria/TalesOfEldoriaClient.java
@@ -0,0 +1,31 @@
+package net.sepinula.talesofeldoria;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = TalesOfEldoria.MOD_ID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = TalesOfEldoria.MOD_ID, value = Dist.CLIENT)
+public class TalesOfEldoriaClient {
+    public TalesOfEldoriaClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        TalesOfEldoria.LOGGER.info("HELLO FROM CLIENT SETUP");
+        TalesOfEldoria.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
